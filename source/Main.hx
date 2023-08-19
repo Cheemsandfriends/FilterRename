85633ae19f726ddf0082929171da2792baa68125
CheemsAndFriends <granchinvictor@gmail.com>
2023-08-19 23:59:15 +0200

    Finish some cool stuff

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,16 @@
 package;
 
 import flixel.FlxGame;
+import lime.app.Application;
 import openfl.display.Sprite;
+import openfl.events.EventType;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState.new));
+		addChild(new FlxGame(0, 0, PlayState));
+		trace(stage.stageWidth);
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -12,9 +12,12 @@ class PlayState extends FlxState
 		super.create();
 		var eReg = ~/"F": /,
 			eReg2 = ~/(\{([^{}]|(?R))*\})/s,
+			eReg3 = ~/("(.+)")/,
 			source = Assets.getText(AssetPaths.Animation__json);
 
-		var lastMatch = 0, position, array:Array<Array<String>> = [];
+		trace(eReg3.match("\"asdlkajsdlaksjd\":"));
+
+		var lastMatch = 0, position;
 
 		while (eReg.matchSub(source, lastMatch))
 		{
@@ -23,28 +26,48 @@ class PlayState extends FlxState
 			if (eReg2.matchSub(source, position.pos + position.len))
 			{
 				var string = eReg2.matched(0);
+
 				string = string.substring(1, string.length - 1);
-				var filters = [];
-				for (filter in string.split(","))
+
+				var filters = string.split("},");
+
+				var len = 2;
+				var repeated:Map<String, Int> = [];
+				for (filter in filters)
 				{
-					filters.push('{$filter}');
+					var marched = eReg3.matchSub(source, position.pos + len);
+					if (marched)
+					{
+						var filter = eReg3.matched(0);
+						trace(repeated);
+						if (repeated.exists(filter))
+						{
+							var mod = '"${filter.substring(1, filter.length - 1)}_${repeated.get(filter) + 1}"';
+							source = source.substring(0, position.pos + len + 1) + mod + source.substring(position.pos + len + mod.length);
+
+							repeated.set(filter, repeated.get(filter) + 1);
+						}
+						else
+							repeated.set(filter, 0);
+					}
+					len += filter.length;
 				}
-				array.push(filters);
 
 				position = eReg2.matchedPos();
 			}
 
-			lastMatch = position.pos + (position.len + array.length);
+			lastMatch = position.pos + position.len;
 		}
-		trace(array[0]);
 
 		var json:AnimAtlas = Json.parse(source);
-		trace(json.AN.STI.SI.F); // should be an array somehow?
+		trace(json.AN);
+		trace(json.AN.STI.SI.F); // array?? list??
 	}
 }
 
 // I'm not gonna copy the entire texture atlas onto a typedef LMAO
 // Fuck texture atlases, Fuck FlxAnimate and Fuck Adobe Animate for causing me this pain
+
 typedef AnimAtlas =
 {
 	var AN:Animation;
